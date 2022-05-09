4111711e4b249407a91db2e4fd93f09f61eb80bc
AlexeiBaza <AlexeiBazaGM@gmail.com>
2022-05-09 19:53:42 +0300

    change Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -11,39 +11,29 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    private static Connection conn = null;
-    private static Util instance = null;
-
-    private Util() {
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres?serverTimezone=Europe/Moscow&useSSL=false";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "XABP123890";
+    public static Connection connection;
+    static {
         try {
-            if (null == conn || conn.isClosed()) {
-                Properties props = getProps();
-                conn = DriverManager
-                        .getConnection(props.getProperty("db.url"), props.getProperty("db.username"), props.getProperty("db.password"));
-            }
-        } catch (SQLException | IOException e) {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public static Util getInstance() {
-        if (null == instance) {
-            instance = new Util();
-        }
-        return instance;
+    public static Connection getConnection() {
+        return connection;
     }
 
-    public Connection getConnection() {
-        return conn;
-    }
 
-    private static Properties getProps() throws IOException {
-        Properties props = new Properties();
-        try (InputStream in = Files.newInputStream(Paths.get(Util.class.getResource("/database.properties").toURI()))) {
-            props.load(in);
-            return props;
-        } catch (IOException | URISyntaxException e) {
-            throw new IOException("Database config file not found", e);
-        }
-    }
+
+
 }
