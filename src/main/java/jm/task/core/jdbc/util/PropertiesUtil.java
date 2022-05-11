7843a5f84cbfa11ca6749e26fab9d25d773c4b32
AlexeiBaza <AlexeiBazaGM@gmail.com>
2022-05-11 23:01:45 +0300

    last commit

---
+++ src/main/java/jm/task/core/jdbc/util/PropertiesUtil.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.util;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public final class PropertiesUtil {
+    private static final Properties PROPERTIES = new Properties();//сюда загружаем все файылы из resources
+
+    static {
+        loadProperties();
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);//метод возвращающий properties по ключу, по сути value из ассоциативного массива
+    }
+
+    private static void loadProperties() {
+        try (InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties");) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);//чтобы приложение упало если не прочтет application.properties файл
+        }
+    }
+
+    private PropertiesUtil() {
+    }
+}
